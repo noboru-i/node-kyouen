71e044269fdad46d154aefb073c02c5224674274
noboru-i <ishikura.noboru@gmail.com>
2018-07-10 23:29:03 +0900

    use prettier / tslint

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 /**
-* @Method: Returns the uppercase string.
-* @Param {string}
-* @Return {string}
-*/
-export function getUpperCase (str: string) : string {
+ * @Method: Returns the uppercase string.
+ * @Param {string}
+ * @Return {string}
+ */
+export function getUpperCase(str: string): string {
   return str.toUpperCase()
 }
